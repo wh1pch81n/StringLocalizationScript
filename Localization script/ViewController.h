4cdeb39ac2a9f5f8deb573e6e0cbe908136d6c9d
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2016-11-13 12:15:28 -0500

    Initial Commit

---
+++ Localization script/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Localization script
+//
+//  Created by Derrick Ho on 11/13/16.
+//  Copyright © 2016 Derrick Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
